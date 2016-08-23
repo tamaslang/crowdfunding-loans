cd00d7c50b8e35076ac5fec4e7d1f2372f6bad48
Tamas Lang <tamas.lang@talangsoft.org>
2016-08-23 15:24:06 +0200

    simplify code

--- src/main/java/org/talangsoft/crowdfunding/service/LendingCalculationLowestRateFirstStrategy.java
+++ src/main/java/org/talangsoft/crowdfunding/service/LendingCalculationLowestRateFirstStrategy.java
@@ -5,9 +5,11 @@ import org.talangsoft.crowdfunding.model.LoanOffer;
 import org.talangsoft.crowdfunding.model.RequestAndOffers;
 
 import java.math.BigDecimal;
+import java.util.Iterator;
 
 public class LendingCalculationLowestRateFirstStrategy implements LendingCalculationStrategy {
     private CurrentOfferResult NO_OFFER = new CurrentOfferResult(BigDecimal.ZERO, BigDecimal.ZERO);
+
     @Override
     public CurrentOfferResult calculateCurrentOffer(RequestAndOffers requestAndOffers) {
         if (requestAndOffers.getOffers().isEmpty()) {
@@ -16,12 +18,12 @@ public class LendingCalculationLowestRateFirstStrategy implements LendingCalcula
         BigDecimal remainderFromRequest = requestAndOffers.getRequest().getAmount();
         BigDecimal weightedInterest = BigDecimal.ZERO;
 
-        for (LoanOffer offer : requestAndOffers.getOffers()) {
-            if (remainderFromRequest.compareTo(BigDecimal.ZERO) > 0) {
-                BigDecimal availableToTake = remainderFromRequest.min(offer.getAmount());
-                remainderFromRequest = remainderFromRequest.subtract(availableToTake);
-                weightedInterest = weightedInterest.add(availableToTake.multiply(offer.getInterestRate()));
-            } else break;
+        Iterator<LoanOffer> offerIt = requestAndOffers.getOffers().iterator();
+        while (offerIt.hasNext() && remainderFromRequest.compareTo(BigDecimal.ZERO) > 0) {
+            LoanOffer offer = offerIt.next();
+            BigDecimal availableToTake = remainderFromRequest.min(offer.getAmount());
+            remainderFromRequest = remainderFromRequest.subtract(availableToTake);
+            weightedInterest = weightedInterest.add(availableToTake.multiply(offer.getInterestRate()));
         }
         BigDecimal amountSatisfied = requestAndOffers.getRequest().getAmount().subtract(remainderFromRequest);
         return new CurrentOfferResult(amountSatisfied, weightedInterest.divide(amountSatisfied));
